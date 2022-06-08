6bd77b0d96dee062d541681fc394c75bdd824a62
derek.weber <derek.weber@NMC-CLG-2447>
2022-06-08 11:49:02 -0700

    Add project files.

---
+++ ServerBackend/Controllers/ProblemController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using System.Web.Http;
+
+namespace ServerBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProblemController : ControllerBase
+    {
+        [HttpGet(Name = "GetProblem")]
+        public string Get()
+        {
+            return "problems";
+        }
+
+        [HttpPost(Name = "PostProblem")]
+        public string Post(string request)
+        {
+            return request;
+        }
+    }
+}
